3798a6d4c405d862bfce5956c20af15648c8321a
sameer rasa <crazzywizard@gmail.com>
2024-02-24 18:03:28 -0700

    fixing cron job

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,17 @@ import axios from 'axios';
 import { createClient } from 'redis';
 
 cron.schedule(`*/5 * * * *`, async () => {
+  console.log(`Running your task...`);
   const client = createClient({ url: process.env.REDIS_URL });
+  console.log(`Connected to Redis...`);
+  console.log(`Fetching data from QuickIndexer...`);
   const responses = await Promise.all([
-    axios.get('https://api.quickindexer.com/leaderboard?days=1'),
-    axios.get('https://api.quickindexer.com/leaderboard?days=7'),
-    axios.get('https://api.quickindexer.com/leaderboard?days=30'),
+    axios.get('https://api.quickindexer.xyz/leaderboard?days=1'),
+    axios.get('https://api.quickindexer.xyz/leaderboard?days=7'),
+    axios.get('https://api.quickindexer.xyz/leaderboard?days=30'),
   ]);
+  console.log(`Data fetched from QuickIndexer...`);
+  console.log(`Storing data in Redis...`);
   const leaderBoard = {
     '1': responses[0].data,
     '7': responses[1].data,
